0b1cdf1b4cd2f5cc2a190a906801edf5f6b3a77a
Yoonsuh Kim <blawesomek@gmail.com>
2024-12-08 22:47:08 +0900

    commit

---
+++ bin/dart_console_01.dart
@@ -0,0 +1,5 @@
+import 'package:dart_console_01/dart_console_01.dart' as dart_console_01;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_console_01.calculate()}!');
+}
---
+++ test/dart_console_01_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_console_01/dart_console_01.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
